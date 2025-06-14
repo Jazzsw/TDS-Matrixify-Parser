a5a09bf72a6c61ac95dd03decf4b02e9c9c6c85d
Jazz Stocker-Witterick <jstocker@uoguelph.ca>
2025-06-14 16:27:29 -0400

    app styling and basic setup + font file

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron/main')
+
+//Simple window creation with browserWindow loading layout from index 
+  const createWindow = () => {
+  const win = new BrowserWindow({//define window size
+     // remove the default titlebar
+    titleBarStyle: 'hidden',
+    // expose window controls in Windows/Linux
+    ...(process.platform !== 'darwin' ? { titleBarOverlay: true } : {})
+  })
+
+  win.loadFile('src/index.html')
+}
+
+//MacOS window managment  
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+//Windows and Linux operating system process killer when windows are closed
+app.on('window-all-closed', () => {
+  //if (process.platform !== 'darwin') {
+    app.quit()
+  //}
+})
