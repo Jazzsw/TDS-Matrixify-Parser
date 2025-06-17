c1e18ee91bd590acdddf8c7b1527b7bd489fbf61
Jazz Stocker-Witterick <jstocker@uoguelph.ca>
2025-06-17 16:38:46 -0400

    command mode and tags area added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-// Item Override Section
+//Triggers
+const node = document.getElementById("overridePrice");
+node.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        let code = document.getElementById("overrideCode").value;
+        let price = document.getElementById("overridePrice").value;
+
+        if(code != "" && price != ""){
+          var newText = document.createTextNode(code + "- $" + price);
+          var newElement = document.createElement("div");
+          newElement.style.color = "#ccc"
+
+          newElement.appendChild(newText)
+          document.getElementById("overridesWrapper").appendChild(newElement);
+
+          document.getElementById("overrideCode").value = ""
+          document.getElementById("overridePrice").value = ""
+  } 
+        
+    }
+});
+
 document.getElementById("overrideBtn").addEventListener('click', function(){
-  let code = document.getElementById("overrideCode").value;
+  
+ let code = document.getElementById("overrideCode").value;
   let price = document.getElementById("overridePrice").value;
 
-  var newText = document.createTextNode(code + "- $" + price);
-  var newElement = document.createElement("div");
-  newElement.style.color = "#ccc"
+  if(code != "" && price != ""){
+    var newText = document.createTextNode(code + "- $" + price);
+    var newElement = document.createElement("div");
+    newElement.style.color = "#ccc"
 
-  newElement.appendChild(newText)
-  document.getElementById("overridesWrapper").appendChild(newElement);
+    newElement.appendChild(newText)
+    document.getElementById("overridesWrapper").appendChild(newElement);
 
-  document.getElementById("overrideCode").value = ""
-  document.getElementById("overridePrice").value = ""
-})  
+    document.getElementById("overrideCode").value = ""
+    document.getElementById("overridePrice").value = ""
+  } 
+})
+
+//swap focus to the second input
+document.getElementById("overrideCode").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        document.getElementById("overridePrice").focus();
+    }
+});
+
