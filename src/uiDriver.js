00df17c33d65f36707cf8805609beff43ce93cb7
Jazz Stocker-Witterick <jstocker@uoguelph.ca>
2025-06-18 13:52:19 -0400

    file list added

--- src/uiDriver.js
+++ src/uiDriver.js
@@ -1,3 +1,5 @@
+import ExcelJS from 'https://cdn.jsdelivr.net/npm/exceljs/+esm';
+
 // Price Override
 let overridesCount = 0;
 
@@ -161,3 +163,84 @@ document.getElementById("tagBtn").addEventListener('click', function(){
             tagsCount++;
   } 
 })
+
+
+
+let filesCount = 0;
+
+//File Upload
+var dropZone = document.getElementById("dropBox")
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dropZone.classList.add('hover');
+})
+
+dropZone.addEventListener('dragleave', () => {
+    dropZone.classList.remove('hover');
+});
+
+const c = document.getElementById('consoleText');
+c.innerHTML = "Waiting for file...";
+
+
+dropZone.addEventListener('change', async (event) => {
+    event.preventDefault();
+    dropZone.classList.remove('hover');
+    c.innerHTML = "Reading file...";
+
+
+    const file = event.target.files[0];
+    if (!file) {
+      c.innerHTML = "No file selected.";
+      return;
+    }
+
+    c.innerHTML = (file.name)
+    addFile(file.name)
+
+    const buffer = await file.arrayBuffer();
+
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.load(buffer);
+
+  });
+
+  function addFile(filename){
+        let parent = document.getElementById("filesWrapper")
+
+        let newContainer = document.createElement("div")
+        let newText = document.createTextNode(filename)
+        let emptyText = document.getElementById("filesEmptyText")
+        newContainer.className = "fileBar"
+
+        let newDropdown = document.createElement("select");
+        //add options
+
+        const deleteBtn = document.createElement('span');
+        deleteBtn.className = 'deleteTag';
+        deleteBtn.textContent = 'Delete';
+
+        deleteBtn.addEventListener('click', () => {
+                newContainer.remove();
+                filesCount--;
+                if(filesCount == 0){
+                    emptyText.style.display = "block"
+                }
+            });
+
+        newContainer.appendChild(newText);
+        newContainer.appendChild(newDropdown);
+        newContainer.appendChild(deleteBtn);
+
+        parent.appendChild(newContainer)
+
+
+        filesCount++;
+        emptyText.style.display = "none"
+
+
+
+
+
+  }
+
