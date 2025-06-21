1fe99c1afe5d5cf23f79a3c82f39a4b771215b20
Jazz Stocker-Witterick <jstocker@uoguelph.ca>
2025-06-21 13:37:26 -0400

    working formation and download for xlsx files

--- src/parser.js
+++ src/parser.js
@@ -2,10 +2,13 @@ import ExcelJS from 'https://cdn.jsdelivr.net/npm/exceljs/+esm';
 import { tagsArr } from './uiDriver.js';
 import { overridesArr } from './uiDriver.js';
 import { fileObjList } from './uiDriver.js';
+import { saveAs } from 'https://cdn.skypack.dev/file-saver';
 
 document.getElementById("createFile").addEventListener('click', function(){
 
     let commandMode = document.getElementById("commandSelect").value
+
+    console.log(JSON.stringify(tagsArr));
 
     let filePairings =[]
 
@@ -32,48 +35,56 @@ document.getElementById("createFile").addEventListener('click', function(){
 
 async function parseBM(file, mode){
 
+        const downloadWorkbook = new ExcelJS.Workbook();
+        const downloadSheet = downloadWorkbook.addWorksheet('Products');
+        downloadSheet.columns = [
+        { header: 'Variant SKU', key: 'sku', width: 30 },
+        { header: 'Variant Price', key: 'price', width: 10 },
+        { header: 'Command', key: 'command', width: 10},
+        { header: 'Tags', key: 'tags', width: 10},
+        { header: 'Tags Command', key: 'tagscommand', width: 15}
+
+        ];
+
         const buffer = await file.arrayBuffer();
 
         const workbook = new ExcelJS.Workbook();
         await workbook.xlsx.load(buffer);
 
         workbook.eachSheet((worksheet) => {
-        alert(worksheet.name)
+        //alert(worksheet.name);
         
         worksheet.eachRow((row, rowNumber) => {
             let SKU = row.values[1] // find SKU
             let lastIndex = SKU.lastIndexOf("-"); //remove and convert the postfix code 
             let code = SKU.slice(lastIndex);
-            let prefix = SKU.slice(0,lastIndex)
+            let prefix = SKU.slice(0,lastIndex);
+            let price = row.values[6].result
             
 
-            SKU = replaceCode(prefix, code)
-            SKU = "BM-".concat(SKU)
-            console.log(SKU)
+            SKU = replaceCode(prefix, code);
+            SKU = "BM-".concat(SKU);
+            
+            if(SKU.length < 50){
+                console.log(arrToStr(tagsArr))
+                downloadSheet.addRow({sku: SKU, price: price, command: mode, tagscommand: "MERGE", tags: arrToStr(tagsArr)});
+            }
 
+            //console.log(SKU);
 
-
-            //c.innerHTML += `<br>Row ${rowNumber}: ${row.values.slice(1).join(', ')}`;
-            //c.innerHTML += `<br>Row ${rowNumber}: ${row.values[6].result}`;
         });
 
         })
 
-        // const worksheet = workbook.addWorksheet('Products');
-        // worksheet.columns = [
-        // { header: 'Variant SKU', key: 'Variant SKU', width: 10 },
-        // { header: 'Variant Price', key: 'Variant Price', width: 10 },
-        // { header: 'Tags', key: 'Tags', width: 10}
-        // ];
 
-        // if(tagsArr.length > 0){
-        //     (worksheet.columns).push({ header: 'Variant SKU', key: 'Variant SKU', width: 10 })
-        // }
-
-
-
-
-
+        downloadWorkbook.xlsx.writeBuffer().then((buffer) => {
+          const blob = new Blob([buffer], {
+            type:
+              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+          });
+        
+          saveAs(blob, 'test.xlsx'); // Trigger download
+        });
 
 }
 
@@ -96,11 +107,27 @@ function replaceCode(SKU, code){
         case "-PN":
             return SKU.concat("-C14");
             break;
-        case "SN":
+        case "-BN":
             return SKU.concat("-C15");
             break;
+        case "-PL":
+            return SKU.concat("-C3");
+            break;
+        case "-20":
+            return SKU.concat(code)
         default:
-            return "UNKNOWN SKU POSTFIX"
+            if(code[0] != "-"){
+                return SKU;
+            }
+            else{
+                console.log("%c SKU NOT CONVERTED " + SKU.concat(code), "color: red; font-weight: bold;");
+                return SKU.concat(code)
+            }
     }
 
+}
+
+
+function arrToStr(array){
+    return array.join(",");
 }
